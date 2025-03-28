8beee3d9cf7444b8b60e41a9a3b0ba3dc4525257
Leonardo Ranoesendjojo <125569752+LeoRanoe@users.noreply.github.com>
2025-03-28 09:45:22 -0300

    Update configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Web-Portfolio/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   server: {
     port: 3002,
     open: true
